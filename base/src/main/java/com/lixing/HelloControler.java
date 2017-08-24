fddbac7063e5d408e350766dddd91a907651419d
lixing <369431735@qq.com>
2017-08-24 18:04:11 +0800

    base demo

---
+++ base/src/main/java/com/lixing/HelloControler.java
@@ -0,0 +1,25 @@
+package com.lixing;
+
+import com.lixing.GirlProperties;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by Administrator on 2017/1/30.
+ */
+@RestController
+public class HelloControler {
+
+    @Value("${cupsize}")
+    private String cupsize;
+    @Autowired
+    private GirlProperties girlProperties;
+
+    @RequestMapping(value = "/")
+    public String say(String id) {
+        return "cupsize======"+cupsize+".........girlProperties="+girlProperties.toString();
+    }
+
+}

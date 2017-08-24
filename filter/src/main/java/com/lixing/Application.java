97aae007ca41c189c66a3c38810c8b40a721a848
lixing <369431735@qq.com>
2017-08-24 16:14:28 +0800

    完成filter demo

--- filter/src/main/java/com/lixing/Application.java
+++ filter/src/main/java/com/lixing/Application.java
@@ -1,10 +1,7 @@
-package com.didispace;
+package com.lixing;
 
-import org.apache.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 public class Application {

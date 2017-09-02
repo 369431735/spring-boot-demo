fa5d71c828ed232ccee230a9750839d10b6257af
lixing <369431735@qq.com>
2017-09-02 10:49:11 +0800

    新增webSocket demo

---
+++ websocket/src/main/java/com/lixing/broadcast/RequestMessage.java
@@ -0,0 +1,13 @@
+package com.lixing.broadcast;
+
+/**
+ * Created by sang on 16-12-22.
+ */
+public class RequestMessage {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+}

6275e2927b64f4ba34a692cb287f199f5105c583
lixing <369431735@qq.com>
2017-09-14 20:47:39 +0800

    修改配置

--- security/src/main/java/com/lixing/WebSecurityConfig.java
+++ security/src/main/java/com/lixing/WebSecurityConfig.java
@@ -1,5 +1,9 @@
 package com.lixing;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -10,19 +14,40 @@ import org.springframework.security.core.userdetails.UserDetailsService;
  */
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
-    UserDetailsService userService(){
-        return  new UserService();
+    UserDetailsService userService() {
+        return new UserService();
     }
 
+    @Bean
     @Override
-    public void configure(WebSecurity web) throws Exception {
-        super.configure(web);
+    protected AuthenticationManager authenticationManager() throws Exception {
+        return super.authenticationManager();
     }
+
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        super.configure(http);
-    http.authorizeRequests().anyRequest().authenticated().
-            and().formLogin().loginPage("/login").failureForwardUrl("/login?error").permitAll().
-            and().logout().permitAll();}
+        http.authorizeRequests()
+                .antMatchers("/", "/home").permitAll()
+                //其他地址的访问均需验证权限
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                //指定登录页是"/login"
+                .loginPage("/login")
+                .defaultSuccessUrl("/hello")//登录成功后默认跳转到"/hello"
+                .permitAll()
+                .and()
+                .logout()
+                .logoutSuccessUrl("/home")//退出登录后的默认url是"/home"
+                .permitAll();
+
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+
+        auth .userDetailsService(userDetailsService());
+
+    }
 }
